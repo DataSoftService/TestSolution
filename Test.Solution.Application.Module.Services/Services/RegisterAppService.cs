26731a0c0d03b88b4e49c596a35922a1696e7225
Alberto Reyes Peralta <areyes@coopaltagracia.com>
2022-12-21 12:41:23 -0400

    service advance

--- Test.Solution.Application.Module.Services/Services/RegisterAppService.cs
+++ Test.Solution.Application.Module.Services/Services/RegisterAppService.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Acv2.SharedKernel.Application;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using TestSolution.Application.DTOs.Dto;
+using TestSolution.Domain.Models;
 using TestSolution.Infrastructure.Database.Communication;
 
 namespace Test.Solution.Application.Module.Services.Services
@@ -15,57 +17,78 @@ namespace Test.Solution.Application.Module.Services.Services
             db = _db;
         }
 
-        public Task<bool> delete(RegisterDto entity, int id)
+        public async Task<bool> delete(RegisterDto entity, int id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> delete(RegisterDto entity)
+        public async Task<bool> delete(RegisterDto entity)
         {
             throw new NotImplementedException();
         }
 
-        public Task<RegisterDto> edit(RegisterDto entity)
+        public async Task<RegisterDto> edit(RegisterDto entity)
+        {
+            try
+            {
+                db.register.Update(entity.ProjectedAs<Register>());
+                db.SaveChanges();
+                return await Task.FromResult(entity);
+            }
+            catch (Exception x)
+            {
+                throw new ArgumentException(x.Message);
+            }
+        }
+
+        public async Task<RegisterDto> edit(string sql)
         {
             throw new NotImplementedException();
         }
 
-        public Task<RegisterDto> edit(string sql)
+        public async Task<RegisterDto> find(int id)
+        {
+            try
+            {
+                var entity = db.register.Find(id);
+                if (entity == null)
+                {
+                    entity = new Register();
+                }
+                return await Task.FromResult(entity.ProjectedAs<RegisterDto>());
+            }
+            catch (Exception x)
+            {
+                throw new ArgumentException(x.Message);
+            }
+        }
+
+        public async Task<RegisterDto> find(Func<RegisterDto, bool> lanbdaExpresion)
         {
             throw new NotImplementedException();
         }
 
-        public Task<RegisterDto> find(int id)
+        public async Task<RegisterDto> insert(RegisterDto entity)
         {
             throw new NotImplementedException();
         }
 
-        public Task<RegisterDto> find(Func<RegisterDto, bool> lanbdaExpresion)
+        public async Task<RegisterDto> insert(IEnumerable<RegisterDto> list)
         {
             throw new NotImplementedException();
         }
 
-        public Task<RegisterDto> insert(RegisterDto entity)
+        public async Task<IEnumerable<RegisterDto>> list()
         {
             throw new NotImplementedException();
         }
 
-        public Task<RegisterDto> insert(IEnumerable<RegisterDto> list)
+        public async Task<IEnumerable<RegisterDto>> list(Func<RegisterDto, bool> lanbdaExpresion)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<RegisterDto>> list()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<RegisterDto>> list(Func<RegisterDto, bool> lanbdaExpresion)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<RegisterDto>> list(int id)
+        public async Task<IEnumerable<RegisterDto>> list(int id)
         {
             throw new NotImplementedException();
         }
